c120fa7f2867806744b800aa80254e2cc64545c9
Kyle Corry <kylecorry31@gmail.com>
2025-03-03 21:10:10 -0500

    Generate kotlin file

--- src/functions.rs
+++ src/functions.rs
@@ -1,11 +1,38 @@
+use std::collections::HashMap;
+
 use crate::{
     config::Member,
-    names::{get_modules, get_unqualified_name, to_camel_case},
-    types::get_jni_type,
+    names::{get_modules, get_unqualified_name, to_camel_case, to_pascal_case},
+    types::{get_jni_type, get_kotlin_type},
 };
 
+const JNI_FUNCTION_TEMPLATE: &str = r#"#[unsafe(no_mangle)]
+pub extern "C" fn {name}(
+    mut env: JNIEnv,
+    _: JClass,
+{params}
+){ret_type} {
+    {result_assignment}{func_name}({args});
+    {return}
+}"#;
+
+const KOTLIN_FUNCTION_TEMPLATE: &str = r#"external fun {name}({params}){ret_type}"#;
+
+fn populate_template(template: &str, parameters: &HashMap<String, String>) -> String {
+    let mut result = template.to_string();
+    for (key, value) in parameters {
+        result = result.replace(&format!("{{{}}}", key), value);
+        println!("{}", result);
+    }
+    result
+}
+
 fn get_jni_function_name(java_package: &str, name: &str) -> String {
-    let module_names = get_modules(name).join("_");
+    let module_names = get_modules(name)
+        .iter()
+        .map(|m| to_pascal_case(m))
+        .collect::<Vec<_>>()
+        .join("_");
     let function_name = to_camel_case(&get_unqualified_name(name));
     format!(
         "Java_{}_{}_{}",
@@ -30,29 +57,83 @@ pub fn generate_jni_function(java_package: &str, function: &Member) -> String {
 
     let unqualified_name = function.name.split("::").last().unwrap();
 
-    format!(
-        "#[unsafe(no_mangle)]\npub extern \"C\" fn {}(\n    mut env: JNIEnv,\n    _: JClass,\n{}\n){} {{\n    {}{}({});\n    {}\n}}",
-        get_jni_function_name(java_package, &function.name),
-        inputs.join(",\n"),
-        output
-            .as_ref()
-            .map_or(String::new(), |o| format!(" -> {}", o)),
-        if output.is_some() {
-            "let result = "
-        } else {
-            ""
-        },
-        unqualified_name,
-        function
-            .inputs
-            .as_ref()
-            .unwrap_or(&vec![])
-            .iter()
-            .map(|i| i.name.as_ref())
-            .collect::<Vec<_>>()
-            .join(", "),
-        if output.is_some() { "result" } else { "" }
-    )
+    let params = HashMap::from([
+        (
+            "name".to_string(),
+            get_jni_function_name(java_package, &function.name),
+        ),
+        ("params".to_string(), inputs.join(",\n")),
+        (
+            "ret_type".to_string(),
+            output
+                .as_ref()
+                .map_or(String::new(), |o| format!(" -> {}", o)),
+        ),
+        (
+            "result_assignment".to_string(),
+            if output.is_some() {
+                "let result = "
+            } else {
+                ""
+            }
+            .to_string(),
+        ),
+        ("func_name".to_string(), unqualified_name.to_string()),
+        (
+            "args".to_string(),
+            function
+                .inputs
+                .as_ref()
+                .unwrap_or(&vec![])
+                .iter()
+                .map(|i| i.name.as_ref())
+                .collect::<Vec<_>>()
+                .join(", "),
+        ),
+        (
+            "return".to_string(),
+            if output.is_some() { "result" } else { "" }.to_string(),
+        ),
+    ]);
+
+    populate_template(JNI_FUNCTION_TEMPLATE, &params)
+}
+
+pub fn generate_kotlin_function(function: &Member) -> String {
+    let inputs = function
+        .inputs
+        .as_ref()
+        .unwrap_or(&vec![])
+        .iter()
+        .map(|i| format!("{}: {}", i.name, get_kotlin_type(&i.rust_type)))
+        .collect::<Vec<_>>()
+        .join(", ");
+
+    let output = function
+        .output
+        .as_ref()
+        .map(|t| format!(": {}", get_kotlin_type(t)))
+        .unwrap_or_default();
+
+    // let unqualified_name = function.name.split("::").last().unwrap();
+    let module_names = get_modules(&function.name)
+        .iter()
+        .skip(1)
+        .map(|m| to_pascal_case(m))
+        .collect::<Vec<_>>()
+        .join("_");
+    let function_name = to_camel_case(&get_unqualified_name(&function.name));
+
+    let name = format!("{}_{}", module_names, function_name);
+
+    let params = HashMap::from([
+        ("name".to_string(), name.clone()),
+        ("params".to_string(), inputs),
+        ("ret_type".to_string(), output.clone()),
+        ("func_name".to_string(), name),
+    ]);
+
+    populate_template(KOTLIN_FUNCTION_TEMPLATE, &params)
 }
 
 #[cfg(test)]
@@ -63,9 +144,9 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case("com.example", "test::func", "Java_com_example_test_func")]
-    #[case("com.example_test", "test::func", "Java_com_example_1test_test_func")]
-    #[case("com.example", "mod1::mod2::func", "Java_com_example_mod1_mod2_func")]
+    #[case("com.example", "test::func", "Java_com_example_Test_func")]
+    #[case("com.example_test", "test::func", "Java_com_example_1test_Test_func")]
+    #[case("com.example", "mod1::mod2::func", "Java_com_example_Mod1_Mod2_func")]
     fn test_jni_function_name(#[case] package: &str, #[case] name: &str, #[case] expected: &str) {
         assert_eq!(get_jni_function_name(package, name), expected);
     }
@@ -83,7 +164,7 @@ mod tests {
         };
 
         let expected = r#"#[unsafe(no_mangle)]
-pub extern "C" fn Java_com_example_test_func(
+pub extern "C" fn Java_com_example_Test_func(
     mut env: JNIEnv,
     _: JClass,
     arg1: jint
--- src/types.rs
+++ src/types.rs
@@ -14,6 +14,22 @@ pub fn get_jni_type(rust_type: &str) -> String {
     String::from(jtype)
 }
 
+pub fn get_kotlin_type(rust_type: &str) -> String {
+    let kotlin_type = match rust_type {
+        "i8" => "Byte",
+        "i16" => "Short",
+        "i32" => "Int",
+        "i64" => "Long",
+        "f32" => "Float",
+        "f64" => "Double",
+        "bool" => "Boolean",
+        "char" => "Char",
+        // TODO: Handle arrays, strings, and objects
+        _ => panic!("Unsupported type: {}", rust_type),
+    };
+    String::from(kotlin_type)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -30,6 +46,19 @@ mod tests {
     #[case("char", "jchar")]
     fn test_get_jni_type(#[case] input: &str, #[case] expected: &str) {
         assert_eq!(get_jni_type(input), expected);
+    }
+
+    #[rstest]
+    #[case("i8", "Byte")]
+    #[case("i16", "Short")]
+    #[case("i32", "Int")]
+    #[case("i64", "Long")]
+    #[case("f32", "Float")]
+    #[case("f64", "Double")]
+    #[case("bool", "Boolean")]
+    #[case("char", "Char")]
+    fn test_get_kotlin_type(#[case] input: &str, #[case] expected: &str) {
+        assert_eq!(get_kotlin_type(input), expected);
     }
 
     #[rstest]
